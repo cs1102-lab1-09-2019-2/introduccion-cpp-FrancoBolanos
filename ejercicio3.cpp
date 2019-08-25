671b0e69be7c7d6f6c80f537889dab9007383a1e
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-25 01:10:12 -0500

    ejercicio3 resuelto

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+
 using std::cout;
 
 int main() {
-    for (int i = 0; i < ; i++) {
+    for (int i = 0; i <= 9; i=++i){
+        cout<<i;
         //imprimir los 10 numeros [0-9]
     }
+    return 0;
 }
