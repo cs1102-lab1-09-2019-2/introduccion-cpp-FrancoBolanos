4114531e7023535a5ebd08b64110306c9f44c97b
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-25 01:13:25 -0500

    ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,13 +1,18 @@
+#include <iostream>
+#include <vector>
 
+using std::cout;
+using std::vector;
 
 int main() {
     vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
     for (auto a : v) {
         for (auto i : a) {
+            std::cout<<i;
             // imprimir i
         }
+        cout<< "\n";
         // salto de linea
     }
+    return 0;
 }
-
-
